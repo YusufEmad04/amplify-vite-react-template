35758adcaf2295756d312b89a9334934c2f78683
yusuf emad <yusufy2004@gmail.com>
2024-05-20 20:00:27 +0300

    edit

--- amplify/functions/python-function/handler.py
+++ amplify/functions/python-function/handler.py
@@ -7,14 +7,14 @@ import boto3
 def lambda_handler(event, context):
     # Retrieve data from the event
     # For example, if the event is a JSON payload:
-    payload = json.loads(event['body'])
+    payload = json.loads(event['body'])\
 
     graphql_api_id = os.environ['GRAPHQL_API_ID']
 
     # connect to graphql appsync
-    client = boto3.client('appsync')
+    # client = boto3.client('appsync')
 
-    r = client.get_graphql_api(apiId=graphql_api_id)
+    # r = client.get_graphql_api(apiId=graphql_api_id)
     
     # Perform your logic here
     # For example, you can process the payload and return a response
@@ -22,10 +22,9 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Origin': '*',  # Replace * with your desired origin
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Methods': '*'  # Add any additional allowed methods
+            'Access-Control-Allow-Headers': '*'
         },
-        'body': json.dumps(r)
+        'body': 'shorse'
     }
     
     return response
